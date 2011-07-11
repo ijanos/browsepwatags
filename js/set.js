37e10d99e2e214dab97223cd488ccdf9e091bbb6
János Illés <ijanos@gmail.com>
2011-07-11 12:43:57 +0200

    Add a simple javascript set

---
+++ js/set.js
@@ -0,0 +1,38 @@
+/*
+A simple JavaScipt Set datasctrcture
+*/
+
+var Set = function(){
+    var set = []
+
+    function unique(item){
+        for(var i=0; i<set.length; i++)
+        {
+            if (set[i] === item) return false;
+        }
+        return true;
+    }
+
+    function add(item){
+        if (item.length)
+        {
+            for(var i=0; i<item.length; i++)
+            {
+                if (unique(item[i])) set.push(item[i])
+            }
+        }
+        else
+        {
+            if (unique(item)) set.push(item[i])
+        }
+    }
+
+    function get(){
+        return set;
+    }
+
+    return {
+        add: add,
+        get: get
+    }
+};
